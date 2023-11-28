3e339e902e5856b993918215458ba51b7428e03a
Bobby-P-dev <bobay772@gmail.com>
2023-11-29 03:58:45 +0700

    fix problem

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"os"
-
 	"github.com/Bobby-P-dev/FinalProject3_kel7/database"
 	"github.com/Bobby-P-dev/FinalProject3_kel7/initiallizers"
 	"github.com/Bobby-P-dev/FinalProject3_kel7/routers"
@@ -12,8 +10,7 @@ func init() {
 	initiallizers.LoadEnvVariable()
 }
 func main() {
-	PORT := os.Getenv("PORT")
 	database.ConnectToDB()
 	r := routers.StarApp()
-	r.Run(PORT)
+	r.Run()
 }
